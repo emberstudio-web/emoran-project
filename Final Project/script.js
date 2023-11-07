55dd1dcfb935dca7512b2ff148c98b41a30a884e
emberstudio-web <emilymoran818@gmail.com>
2023-11-06 19:42:05 -0800

    11/6

---
+++ Final Project/script.js
@@ -0,0 +1,6 @@
+function toggleDropdown(serviceId) {
+    var dropdown = document.getElementById(serviceId);
+    var display = dropdown.style.display;
+    // If dropdown is visible, hide it. Else, show it.
+    dropdown.style.display = display === 'block' ? 'none' : 'block';
+}
